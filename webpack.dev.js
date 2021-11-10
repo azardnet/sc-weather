e310890a40d5a69ed748efda13000d9bb17264c0
sadafamininia99 <aminicomputer96@gmail.com>
2021-11-10 12:14:27 +0330

    svg assests added to file loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -55,7 +55,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
