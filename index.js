0fc180d88980678b3fcb3636128baf0c7500b2c1
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2024-01-03 10:59:21 +0330

    add symbol for wind speed

--- index.js
+++ index.js
@@ -588,8 +588,8 @@ function computeUI(result, city, interval) {
     sl(
       "main .weather .map-overlay .content-wrapper .weather-data .wind-speed .value"
     ).innerHTML = isPersianCharacter
-      ? NumbersToPersian(result.wind.speed.toFixed(TO_FIXED))
-      : result.wind.speed.toFixed(TO_FIXED);
+      ? ` km/h ${NumbersToPersian(result.wind.speed.toFixed(TO_FIXED))}`
+      : `${result.wind.speed.toFixed(TO_FIXED)} km/h`;
     sl(
       ".map-overlay .content-wrapper .weather-data .current-weather-icon span"
     ).innerHTML = result.weather[0].description;
