f1630e287b13ff922940738fe67bbcb6333186b9
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2024-04-22 16:23:45 +0330

    temporary add main-value just html files

--- face/src/App.tsx
+++ face/src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import WeatherData from "./components/WeatherData";
 import { AppWrapper } from "./styled";
 
-
 const OPEN_WEATHER_KEY = import.meta.env.VITE_OPENWEATHER;
 
 function App() {
@@ -46,14 +45,187 @@ https://api.openweathermap.org/data/2.5/weather?lang=en&q=${input}&APPID=${OPEN_
       )} */}
       <div className="t-main">
         <header>
-          <img src="https://azardnet.github.io/sc-weather/img/gb.35dbacd736781608964ae37a3b390b48.svg" width={18} height={18} alt="uk" />
+          <img
+            src="https://azardnet.github.io/sc-weather/img/gb.35dbacd736781608964ae37a3b390b48.svg"
+            width={18}
+            height={18}
+            alt="uk"
+          />
           <h1>Liverpool</h1>
-          <button onClick={() => alert('search')}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 100" fill="none" x="0px" y="0px"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 0H14V14H0V0ZM0 38H14V52H0V38ZM14 76H0V90H14V76Z" fill="black" /></svg>
+          <button onClick={() => alert("search")}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 14 100"
+              fill="none"
+              x="0px"
+              y="0px"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0 0H14V14H0V0ZM0 38H14V52H0V38ZM14 76H0V90H14V76Z"
+                fill="black"
+              />
+            </svg>
           </button>
         </header>
         <section>
-          <div className="temp">26.6°C</div>
+          <div className="temperature">
+            <div className="main-value">
+              <strong>26.6</strong>
+              <span>°C</span>
+            </div>
+            <div className="max-min">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 100 100"
+                >
+                  <path
+                    fill="#ffffff"
+                    d="M50,100c-0.6,0-1-0.4-1-1V2.2c-0.2,0.1-0.4,0.2-0.5,0.4L10.3,40.8c-0.4,0.4-1,0.4-1.4,0s-0.4-1,0-1.4L47.1,1.2  C47.8,0.4,48.9,0,50,0c1.1,0,2.2,0.4,2.9,1.2l38.1,38.1c0.4,0.4,0.4,1,0,1.4s-1,0.4-1.4,0L51.5,2.6c-0.2-0.2-0.3-0.3-0.5-0.4V99  C51,99.6,50.6,100,50,100z"
+                  />
+                </svg>
+                <div>
+                  <strong>21.2</strong>
+                  <span>°C</span>
+                </div>
+              </span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 100 100"
+                >
+                  <path
+                    fill="#ffffff"
+                    d="M50,100c-0.6,0-1-0.4-1-1V2.2c-0.2,0.1-0.4,0.2-0.5,0.4L10.3,40.8c-0.4,0.4-1,0.4-1.4,0s-0.4-1,0-1.4L47.1,1.2  C47.8,0.4,48.9,0,50,0c1.1,0,2.2,0.4,2.9,1.2l38.1,38.1c0.4,0.4,0.4,1,0,1.4s-1,0.4-1.4,0L51.5,2.6c-0.2-0.2-0.3-0.3-0.5-0.4V99  C51,99.6,50.6,100,50,100z"
+                  />
+                </svg>
+                <div>
+                  <strong>20.9</strong>
+                  <span>°C</span>
+                </div>
+              </span>
+            </div>
+            <h3 className="weather-state">few clouds</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+              <defs>
+                <clipPath id="a">
+                  <path
+                    fill="none"
+                    d="M12 35l-5.28-4.21-2-6 1-7 4-5 5-3h6l5 1 3 3L33 20l-6 4h-6l-3 3v4l-4 2-2 2z"
+                  />
+                </clipPath>
+              </defs>
+              <g clip-path="url(#a)">
+                <g>
+                  <path
+                    fill="none"
+                    stroke="#f59e0b"
+                    stroke-linecap="round"
+                    stroke-miterlimit="10"
+                    stroke-width="2"
+                    d="M23.5 24a4.5 4.5 0 11-4.5-4.5 4.49 4.49 0 014.5 4.5zM19 15.67V12.5m0 23v-3.17m5.89-14.22l2.24-2.24M10.87 32.13l2.24-2.24m0-11.78l-2.24-2.24m16.26 16.26l-2.24-2.24M7.5 24h3.17m19.83 0h-3.17"
+                  />
+                  <animateTransform
+                    attributeName="transform"
+                    dur="45s"
+                    from="0 19 24"
+                    repeatCount="indefinite"
+                    to="360 19 24"
+                    type="rotate"
+                  />
+                </g>
+              </g>
+              <path
+                fill="none"
+                stroke="#e5e7eb"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="3"
+                d="M43.67 45.5h2.83a7 7 0 000-14h-.32a10.49 10.49 0 00-19.11-8 7 7 0 00-10.57 6 7.21 7.21 0 00.1 1.14A7.5 7.5 0 0018 45.5a4.19 4.19 0 00.5 0v0"
+              />
+              <g>
+                <path
+                  fill="none"
+                  stroke="#2885c7"
+                  stroke-linecap="round"
+                  stroke-miterlimit="10"
+                  stroke-width="2"
+                  d="M24.39 43.03l-.78 4.94"
+                />
+                <animateTransform
+                  attributeName="transform"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  type="translate"
+                  values="1 -5; -2 10"
+                />
+                <animate
+                  attributeName="opacity"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  values="0;1;1;0"
+                />
+              </g>
+              <g>
+                <path
+                  fill="none"
+                  stroke="#2885c7"
+                  stroke-linecap="round"
+                  stroke-miterlimit="10"
+                  stroke-width="2"
+                  d="M31.39 43.03l-.78 4.94"
+                />
+                <animateTransform
+                  attributeName="transform"
+                  begin="-0.4s"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  type="translate"
+                  values="1 -5; -2 10"
+                />
+                <animate
+                  attributeName="opacity"
+                  begin="-0.4s"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  values="0;1;1;0"
+                />
+              </g>
+              <g>
+                <path
+                  fill="none"
+                  stroke="#2885c7"
+                  stroke-linecap="round"
+                  stroke-miterlimit="10"
+                  stroke-width="2"
+                  d="M38.39 43.03l-.78 4.94"
+                />
+                <animateTransform
+                  attributeName="transform"
+                  begin="-0.2s"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  type="translate"
+                  values="1 -5; -2 10"
+                />
+                <animate
+                  attributeName="opacity"
+                  begin="-0.2s"
+                  dur="0.7s"
+                  repeatCount="indefinite"
+                  values="0;1;1;0"
+                />
+              </g>
+            </svg>
+          </div>
         </section>
       </div>
     </AppWrapper>
--- face/src/styled.ts
+++ face/src/styled.ts
@@ -5,21 +5,22 @@ export const AppWrapper = styled.section`
   height: 100vh;
   align-items: center;
 
-.t-main {
-  background-color: rgb(255 255 255 / 10%);
-  position: relative;
-  width: 325px;
-  height: 220px;
-  box-shadow: 10px 8px 14px -4px rgb(0 0 0 / 16%), 4px 4px 0px 1px rgb(255 255 255 / 10%);
-  border: 1px solid rgb(255 255 255 / 15%);
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-direction: column;
-}
+  .t-main {
+    background-color: rgb(255 255 255 / 10%);
+    position: relative;
+    width: 325px;
+    height: 220px;
+    box-shadow: 10px 8px 14px -4px rgb(0 0 0 / 16%),
+      4px 4px 0px 1px rgb(255 255 255 / 10%);
+    border: 1px solid rgb(255 255 255 / 15%);
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
 
-.t-main:before {
-  content: "";
+  .t-main:before {
+    content: "";
     position: absolute;
     width: 26px;
     height: 26px;
@@ -29,71 +30,76 @@ export const AppWrapper = styled.section`
     border-right: 1px dashed rgba(255, 255, 255, 0.3);
     border-radius: 1px;
     transform: rotate(43deg);
-}
-.t-main header {
-  position: relative;
-  width: 100%;
-  &:before {
-    content: "";
-    position: absolute;
-    top: 18px;
-    width: 100%;
-    height: 1px;
-    background-color: rgb(255 255 255 / 15%);
   }
-  &:after {
-    content: "";
-    position: absolute;
-    top: 0px;
-    background: rgb(255 255 255 / 5%);
-    width: 35px;
-    height: 18px;
-    border: 0;
-    left: 0px;
-    z-index: -1;
-    box-shadow: 7px 0px 8px -6px rgb(0 0 0 / 15%);
-  }
-  img {
-    position: absolute;
-    right: 0px;
-    z-index: -1;
-    top: 0px;
-    height: 18px;
-    opacity: 0.4;
-  }
-  h1 {
-    color: rgb(255, 255, 255);
-    font-size: 24px;
-    line-height: 19px;
-    letter-spacing: 0px;
-    text-align: center;
-    margin: 0px;
-    padding-left: 18px;
-    text-transform: uppercase;
-    text-shadow: 1px 0px 0px rgb(0 0 0 / 30%), 0px 0px 2px rgb(0 0 0 / 11%);
-    width: 80%;
-    overflow: hidden;
-    margin-right: auto;
-    margin-left: auto;
-    text-overflow: ellipsis;
-  }
-  button {
-    position: absolute;
-    top: 0px;
-    background: transparent;
-    width: 35px;
-    height: 18px;
-    border: 0;
-    left: 0px;
-    svg {
-      width: 80%;
-      height: 80%;
-      margin-left: 8px;
-      margin-top: 2px;
-      path {
-        fill: #fff;
+  .t-main {
+    header {
+      position: relative;
+      width: 100%;
+      &:before {
+        content: "";
+        position: absolute;
+        top: 18px;
+        width: 100%;
+        height: 1px;
+        background-color: rgb(255 255 255 / 15%);
+      }
+      &:after {
+        content: "";
+        position: absolute;
+        top: 0px;
+        background: rgb(255 255 255 / 5%);
+        width: 35px;
+        height: 18px;
+        border: 0;
+        left: 0px;
+        z-index: -1;
+        box-shadow: 7px 0px 8px -6px rgb(0 0 0 / 15%);
+      }
+      img {
+        position: absolute;
+        right: 0px;
+        z-index: -1;
+        top: 0px;
+        height: 18px;
+        opacity: 0.4;
+      }
+      h1 {
+        color: rgb(255, 255, 255);
+        font-size: 24px;
+        line-height: 19px;
+        letter-spacing: 0px;
+        text-align: center;
+        margin: 0px;
+        padding-left: 18px;
+        text-transform: uppercase;
+        text-shadow: 1px 0px 0px rgb(0 0 0 / 30%), 0px 0px 2px rgb(0 0 0 / 11%);
+        width: 80%;
+        overflow: hidden;
+        margin-right: auto;
+        margin-left: auto;
+        text-overflow: ellipsis;
+      }
+      button {
+        position: absolute;
+        top: 0px;
+        background: transparent;
+        width: 35px;
+        height: 18px;
+        border: 0;
+        left: 0px;
+        svg {
+          width: 80%;
+          height: 80%;
+          margin-left: 8px;
+          margin-top: 2px;
+          path {
+            fill: #fff;
+          }
+        }
       }
     }
+    section {
+      display: flex;
+    }
   }
-}
 `;
